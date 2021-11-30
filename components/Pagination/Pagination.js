cbaf4268701f56708a713f9a67a8187aa14df8be
Shrey Pandey <shreypandey27@gmail.com>
2021-11-30 23:18:37 +0530

    pagination css updated

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -1,22 +1,11 @@
-import { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
 import { isMobile } from 'react-device-detect';
+import styles from '../../styles/common.module.css';
 
 export default function PaginationBar(){
   let handlePageChange = (data) => {
     //updatePageNumber(data.selected + 1);
   };
-
-  // const [width, setWidth] = useState(window.innerWidth);
-
-  // useEffect(() => {
-  //   window.addEventListener('resize', updateDimensions);
-  //   const updateDimensions = () => {
-  //     setWidth(window.innerWidth);
-  //   };
-  //   return () => window.removeEventListener('resize', updateDimensions);
-  // }, []);
-
 
   return (
     <>
@@ -39,19 +28,20 @@ export default function PaginationBar(){
         `}
       </style>
       <ReactPaginate
-        className='pagination justify-content-center my-4 gap-4'
+        className={styles.pagination}
         nextLabel='Next'
         //forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
         previousLabel='Previous'
-        previousClassName={isMobile ? 'btn btn-outline-primary fs-5 prev' : null}
-        nextClassName={isMobile ? 'btn btn-outline-primary fs-5 next' : null}
-        activeClassName='active'
+        previousClassName={`${isMobile ? styles.dispNone : styles.prev}`}
+        nextClassName={`${isMobile ? styles.dispNone : styles.next}`}
+        activeClassName={styles.active}
+        activeLinkClassName={styles.activeLink}
         marginPagesDisplayed={isMobile ? 1 : 2}
-        pageRangeDisplayed={isMobile ? 1 : 2}
+        pageRangeDisplayed={isMobile ? 1 : 3}
         pageCount={42}
         onPageChange={handlePageChange}
-        pageClassName='page-item'
-        pageLinkClassName='page-link'
+        pageClassName={styles.pageItem}
+        pageLinkClassName={styles.pageLink}
       />
     </>
   );
